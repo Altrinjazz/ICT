2bc0c258967516438c97f22670db7f2490f5085f
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-11-25 22:13:58 +0530

    switch statement ( grade )
    switch statement is used instead of using the if else if  statement for displaying the students grade.

---
+++ switchgradejava.java
@@ -0,0 +1,23 @@
+public class switchgradejava {
+    public static void main(String[] args) {
+        int i = 97, j = 93, k = 95, l = 97, m = 77, avj;
+        avj = (i + j + k + l + m) / 5;
+        System.out.println("Average =" + avj);
+        switch (avj) {
+            case 1:
+                if (avj < 50) {
+                    System.out.println("Average = " + avj + ".    Fail");
+                }
+            case 2:
+                if (avj >= 50 && avj < 60) {
+                    System.out.println("Average = " + avj + ".  Grade : A");
+                }
+            case 3:
+                if (avj >= 60 && avj < 80) {
+                    System.out.println("Average = " + avj + ".   Grade  : O");
+                }
+            default:
+                System.out.println("Average = " + avj + ".   Grade  : S");
+        }
+    }
+}
