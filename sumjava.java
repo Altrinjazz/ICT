cd5ee07f0156dd1170fed98f53828486495561ed
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-11-25 20:25:33 +0530

    java

---
+++ sumjava.java
@@ -0,0 +1,8 @@
+public class sumjava {
+    public static void main(strings[] args) {
+        int i = 1;
+        int j = 3;
+        int sum = i + j;
+        System.out.println("the sum=" + sum);
+    }
+}
