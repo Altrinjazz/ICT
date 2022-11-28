702112c41f5fb06e2123a7198ce514c64520c9c9
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-11-29 05:25:34 +0530

    added brake
    brake is added in the switch

--- switchgradejava.java
+++ switchgradejava.java
@@ -7,14 +7,17 @@ public class switchgradejava {
             case 1:
                 if (avj < 50) {
                     System.out.println("Average = " + avj + ".    Fail");
+                    break;
                 }
             case 2:
                 if (avj >= 50 && avj < 60) {
                     System.out.println("Average = " + avj + ".  Grade : A");
+                    break;
                 }
             case 3:
                 if (avj >= 60 && avj < 80) {
                     System.out.println("Average = " + avj + ".   Grade  : O");
+                    break;
                 }
             default:
                 System.out.println("Average = " + avj + ".   Grade  : S");
