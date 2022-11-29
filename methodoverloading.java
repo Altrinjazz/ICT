bc5e7adad285ec732c83d943753a02b96a6fc50e
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-11-29 22:54:17 +0530

    method over loading.
    multiple methods used in a program. method over loading.

---
+++ methodoverloading.java
@@ -0,0 +1,33 @@
+public class methodoverloading {
+    static String n,a, status,value;
+    static float h,w;
+    methodoverloading()
+    {
+        n="Ram";
+        a="Male";
+        h=45;
+        w=77;
+        System.out.println("Name  : " + n + ";  Gender  : " + a + ";  Height  : " + h + ";  Weight  : " + w);
+    } 
+    public static void main(String[] args) {
+       
+        System.out.println("value :"+ value +"   "+ "Status :"+status);
+        System.out.println("Name  : " + n + ";  Gender  : " + a + ";  Height  : " + h + ";  Weight  : " + w);
+
+    }
+    public String get_details() {
+        return("Name  : " + n + ";  Gender  : " + a + ";  Height  : " + h + ";  Weight  : " + w);
+    }
+    public String get_details( String status) {
+         value=n;
+         if(status==value)
+         {
+             value="Name :"+ n ;
+         }
+         else if (status=="height")
+         {
+             value="Heaight :"+ h;
+         } 
+        return("value"+value +"status"+status);
+    }
+}
