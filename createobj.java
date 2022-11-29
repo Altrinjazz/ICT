740f1bf98d90009d899d9abb9e68bd9a8811afa3
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-11-29 06:26:50 +0530

    Update createobj.java

--- createobj.java
+++ createobj.java
@@ -8,15 +8,3 @@ public class createobj {
 
     }
 }
-
-public class class_obj {
-    public static void main(String[] args) {
-        createobj jazz = new createobj();
-        jazz.name = "ALTRIN JASH .S";
-        jazz.gender = "Maile";
-        jazz.hight = 160;
-        jazz.weight = 60;
-        System.out.println(jazz.get_details());
-
-    }
-}
