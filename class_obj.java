6513607d6646e13dd31fc7676d9a36cd25de819a
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-11-29 14:26:06 +0530

    sp

--- class_obj.java
+++ class_obj.java
@@ -2,7 +2,7 @@ public class class_obj {
     public static void main(String[] args) {
         createobj jazz = new createobj();
         jazz.name = "ALTRIN JASH .S";
-        jazz.gender = "Maile";
+        jazz.gender = "Male";
         jazz.hight = 160;
         jazz.weight = 60;
         System.out.println(jazz.get_details());
---
+++ human.java
@@ -0,0 +1,19 @@
+public class human {
+    public static String name, gender;
+    public static float hight, weight;
+
+    /**
+     * 
+     */
+    human() {
+        name = "Ram";
+        gender = "Male";
+        hight = 160;
+        weight = 60;
+
+    }
+
+    public static void main(strings args[]) {
+        System.out.println("Name  : " + name + ";  Gender  : " + gender + ";  Height  : " + hight + ";  Weight  : " + weight);
+    }
+}
