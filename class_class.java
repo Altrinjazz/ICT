50ecdb8471b0be0c07d969a4816aff645bbc2abf
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-01 15:30:47 +0530

    Create class_class.java

---
+++ class_class.java
@@ -0,0 +1,10 @@
+public class class_class {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("name is name");
+    }
+}
