234d9ad67502a4dc8a4254d3396bb048ed9dace4
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-04 00:30:39 +0530

    Update name.java

--- name.java
+++ name.java
@@ -1,9 +1,11 @@
-public class name() {    
-    public static void main(String[] args) {    
+public class name {
+
+    public static void main(String[] args) {
+        
         int i=1;    
         do{    
             System.out.println(i);    
         i++;    
-        }while(i<=10);    
-    }    
-    }    
+        }while(i<=10);  
+    }
+}
