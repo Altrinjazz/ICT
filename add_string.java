b25eefaf27cbe48bed9db69bf783f32cf4db15a7
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-04 00:55:43 +0530

    Create add_string.java

---
+++ add_string.java
@@ -0,0 +1,8 @@
+public class add_string {
+    public static void main(String args []) {
+     String a="altrin";
+    String b="jash";
+       String c=a+b;
+       System.out.println(c);
+    }
+}
