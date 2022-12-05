12f4e40837c061137b4b1b11ce9e76a0943b7963
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-05 18:13:23 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class array
+{
+    static int i;
+    /**
+     * @param args
+     */
+    public static void main(String[]args) {
+        Scanner obb = new Scanner(System.in);
+        int num[]=new Int(8);
+        int a=num.length;
+        if (i=0;i<num;i++) {
+            
+            System.out.println("please enter the number "+i+"value" );
+        }
+        {
+            System.out.println("please enter the number "+i+"value" );
+        }
+    }
+}
