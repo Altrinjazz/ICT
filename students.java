e0b3987ad82133c00b599c9948616dea70d59b04
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-07 11:57:53 +0530

    aaa

---
+++ students.java
@@ -0,0 +1,12 @@
+public class students {
+    int id;  
+    String name; 
+   public static void main(String[] args) {
+        
+    students s1=new students();
+     System.out.println(s1.id);  
+     System.out.println(s1.name);  
+    }
+        
+    }
+
