6cc43fa05bf04873ac4c15accb2c91bb83e1a6c3
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-15 15:19:34 +0530

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class arrays {
+    public static void main(String[] args) {
+        ArrayList <String> arr = new ArrayList <String> ();
+        arr.add ("hrdgf");
+        arr.add("9846");
+        arr.add("jkc");
+        System.out.println(arr);
+        
+    }
+}
