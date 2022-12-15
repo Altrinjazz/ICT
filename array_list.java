a693aa386ca1d80fd23a10b28a9485aa49b047c3
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-15 15:49:50 +0530

    Update array_list.java

--- array_list.java
+++ array_list.java
@@ -3,7 +3,9 @@ public class array_list {
     public static void main(String[] args) {
         ArrayList <Integer> arr = new ArrayList <Integer> ();
         ArrayList <Integer> arr1 = new ArrayList <Integer> ();
-        arr.add(10,20,30);
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
          int x = 2;
          int i;
         for (i=0;i<3;i++)
