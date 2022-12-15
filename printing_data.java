09608b5d5912ab7258dda4e1db09cf396be84d01
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-15 15:49:36 +0530

    print output
    To print the output without using the system.out.println.

---
+++ printing_data.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+public class printing_data {
+    public static void main(String[] args) {
+        OutputStreamWriter sw= new OutputStreamWriter(System.in);
+        sw.write("hellow.To print the output without using the system.out.println.");
+    }
+}
