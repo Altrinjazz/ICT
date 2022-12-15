872f2a2aacc21f1154c62cebc09acd3ab80cd18b
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-15 23:56:00 +0530

    Update printing_data.java

--- printing_data.java
+++ printing_data.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 public class printing_data {
     public static void main(String[] args) {
-        OutputStreamWriter sw= new OutputStreamWriter(System.in);
+        OutputStreamWriter sw=new OutputStreamWriter(System.in);
         sw.write("hellow.To print the output without using the system.out.println.");
     }
 }
